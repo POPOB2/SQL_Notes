415d43f962ccfcc791b6c1050594d7a147b8ab31
PB2 <nft192891@gmail.com>
2022-06-09 23:02:08 +0800

    找回密碼頁面

---
+++ SQL_login/4_1forgot.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>找回密碼</title>
+</head>
+<body>
+    <h1>找回密碼</h1>
+
+    <form action="4_2chk_acc.php" method="post">
+        <p>請輸入要找回密碼的帳號</p>
+        <input type="text" name="acc">
+        <input type="submit" value="檢查">
+    </form>
+</body>
+</html>
